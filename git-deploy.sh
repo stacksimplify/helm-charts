02194228bfccd107b7101e5b8a45b8ee1cbcfa44
Kalyan Reddy Daida <stacksimplify@gmail.com>
2025-11-30 16:48:40 +0530

    Welcome to StackSimplify

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Add new charts to existing repo - update index"
+helm repo index --url https://stacksimplify.github.io/helm-charts/ --merge index.yaml .
+sleep 10
+
 echo "Add files and do local commit"
 git add .
 git commit -am "Welcome to StackSimplify"
